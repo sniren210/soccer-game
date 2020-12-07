dca20ec4ee732c16525bd5619a4e1674c96c3543
sniren210 <sniren2002@gmail.com>
2020-12-07 19:57:06 +0700

    commit pertama

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1607323388,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/field-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/dragradius-sheet0.png",
+		"images/selectionring-sheet0.png",
+		"images/menubackground-sheet0.png",
+		"images/arrowbutton-sheet0.png",
+		"images/playbutton-sheet0.png",
+		"images/boundingbox-sheet0.png",
+		"images/goalpost-sheet0.png",
+		"images/goalline-sheet0.png",
+		"images/banner-sheet0.png",
+		"images/banner2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
